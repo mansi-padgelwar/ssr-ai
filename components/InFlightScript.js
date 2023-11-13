eedd6c031d4c700f3850c344d5307c7d2bb022d2
mansi-padgelwar <mansi.padgelwar@airindia.com>
2023-11-13 16:14:15 +0530

    add script and urls

---
+++ components/InFlightScript.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+const InFlightScript = () => {
+  useEffect(() => {
+    if (window.InFlight) {
+      console.log("Inflight script loaded successfully");
+      //   window.InFlight.initService(/* parameters */);
+    } else {
+      console.error("InFlight script not loaded successfully.");
+    }
+  }, []);
+
+  return null;
+};
+
+export default InFlightScript;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,36 @@
+import Script from "next/script";
 import "/app/globals.css";
+import InFlightScript from "../components/InFlightScript";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+      {/* <Script src="http://api.inflightpanasonic.aero/inflight.js" />
+      <InFlightScript /> */}
       <h1 className="text-4xl text-blue text-center m-32">Hello World</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 4,
+        }}
+      >
+        <Link
+          href="https://demo-ssr-app-eta.vercel.app/en-us/halloween/app"
+          legacyBehavior
+        >
+          <a>1. Halloween Page</a>
+        </Link>
+        <Link
+          href="https://demo-ssr-app-eta.vercel.app/en-us/diwali/app"
+          legacyBehavior
+        >
+          <a>2. Diwali Page</a>
+        </Link>
+      </div>
     </div>
   );
 }
